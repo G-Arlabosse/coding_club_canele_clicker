7dfc104bf15a2340450c048b9af1edf720dfbb78
Gi-boss <62660428+Gi-boss@users.noreply.github.com>
2020-04-07 16:37:38 +0200

    Employé !

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var can = 0;
-var fours = 0
+var fours = 0;
+var employé = 0;
 var showCan = document.getElementById("nbCanelés");
 var btnFour = document.getElementById("Four");
+var btnEmployé = document.getElementById("Employé");
 
-btnFour.disabled= true;
+btnFour.disabled = true;
+btnEmployé.disabled = true;
 
 function Click(){
     can+=1+fours;
@@ -11,17 +14,39 @@ function Click(){
     showCan.innerHTML=can;
 }
 
-function enableOven(){
+setInterval(Auto, 1000);
+function Auto(){
+    can+=employé;
+    showCan.innerHTML=can;
+    refreshBtn();
+}
+
+function enableBtns(){
     if (can>24){
-        btnFour.disabled= false;
+        btnFour.disabled = false;
+    }
+    if (can>99){
+        btnEmployé.disabled = false;
     }
 }
 
 function buyOven(){
     can-=25;
-    if (can<25){
-        btnFour.disabled= true;
-    }
     fours+=1;
     showCan.innerHTML=can;
+}
+
+function payEmployee(){
+    can-=100;
+    employé+=1;
+    showCan.innerHTML=can;
+}
+
+function refreshBtn(){
+    if (can<25){
+        btnFour.disabled = true;
+    }
+    if (can<100){
+        btnEmployé.disabled = true;
+    }
 }
